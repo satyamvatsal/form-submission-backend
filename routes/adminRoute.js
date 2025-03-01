f601931ce2f976d052ca29208d8080ecc02c3b2e
satyam <satyamvatsal257@gmail.com>
2025-03-01 20:18:03 +0530

    added download csv feature

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fastcsv = require("fast-csv");
 const { returnUsers } = require("../database/queries");
 router.get("/students", async (req, res) => {
   const key = req.headers["admin-key"];
@@ -8,6 +9,27 @@ router.get("/students", async (req, res) => {
     return res.status(200).json({ data });
   }
   return res.status(401).json({ message: "Invalid key" });
+});
+
+router.get("/download-csv", async (req, res) => {
+  try {
+    const key = req.headers["admin-key"];
+    console.log(key);
+    if (key === process.env.ADMIN_KEY) {
+      const data = await returnUsers();
+      res.setHeader("Content-Disposition", "attachment; filename=users.csv");
+      res.setHeader("Content-Type", "text/csv");
+      const csvStream = fastcsv.format({ headers: true });
+      csvStream.pipe(res);
+      if (data.length > 0) data.forEach((row) => csvStream.write(row));
+      csvStream.end();
+    } else {
+      return res.status(401).json({ message: "Invalid key" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
