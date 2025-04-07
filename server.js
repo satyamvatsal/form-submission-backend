01b06688f641accd19e2d3cf81d600ce8ba8e39c
satyam <satyamvatsal257@gmail.com>
2025-04-08 01:30:16 +0530

    added cors for admin site

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://10.3.141.39:5173",
   "https://register.visioncse.tech",
+  "https://admin.visioncse.tech",
 ];
 const corsOptions = {
   origin: function (origin, callback) {
